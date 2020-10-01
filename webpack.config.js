4810d32884028840a475dbcc5864cdd127d25d86
Abhishek Das <abhidas17695@gmail.com>
2020-10-01 16:30:10 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,12 @@ const envs = {
     production: 'prod'
 };
 
+const devServer = {
+    disableHostCheck: true,
+    host: '0.0.0.0',
+    port: 3000
+}
+
 const env = envs[process.env.NODE_ENV || 'development'];
 const envConfig = require(`./config/webpack/webpack.${env}.babel`);
-module.exports = webpackMerge(common, envConfig);
+module.exports = webpackMerge(common, envConfig, devServer);
